bc1fca61856e941f1547d831f80ae720bce302fb
BJCast <89055409+WirelessPresentation@users.noreply.github.com>
2024-04-01 10:00:10 +0800

    Update bj_airplay_def.h

--- zdemo_source_code_for_windows/include/bj_airplay_def.h
+++ zdemo_source_code_for_windows/include/bj_airplay_def.h
@@ -1,10 +1,10 @@
-
+  
 #ifndef BJ_AIRPLAY_DEF_H
 #define BJ_AIRPLAY_DEF_H
 
 typedef enum
 {
-	BJ_AIRPLAY_ERROR_SUCCESS = 0,		   ///成功
+	BJ_AIRPLAY_ERROR_SUCCESS = 0,		   ///鎴愬姛
 	BJ_AIRPLAY_ERROR_PORT_BINDERROR,
 	BJ_AIRPLAY_ERROR_INIT_FAILED,
 	BJ_AIRPLAY_ERROR_INIT_RAOP_FAILED,
@@ -13,11 +13,11 @@ typedef enum
 
 typedef enum
 {
-    BJ_PLAYER_STATUS_LOADING = 0,    //���ڼ���
-    BJ_PLAYER_STATUS_PLAYING,    //���ڲ���
-    BJ_PLAYER_STATUS_PAUSED,     //��ͣ״̬
-    BJ_PLAYER_STATUS_ENDED,      //���Ž���
-    BJ_PLAYER_STATUS_FAILED,     //����ʧ��
+    BJ_PLAYER_STATUS_LOADING = 0,    //正在加载
+    BJ_PLAYER_STATUS_PLAYING,    //正在播放
+    BJ_PLAYER_STATUS_PAUSED,     //暂停状态
+    BJ_PLAYER_STATUS_ENDED,      //播放结束
+    BJ_PLAYER_STATUS_FAILED,     //播放失败
 }BJAirPlayerStatus;
 
 #endif
