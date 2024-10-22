2d4a93dfb46717b13bbc513a0dc302d8e6afd798
BJCast <89055409+WirelessPresentation@users.noreply.github.com>
2024-10-22 15:58:38 +0800

    Update gradlew.bat

--- zdemo_source_code/gradlew.bat
+++ zdemo_source_code/gradlew.bat
@@ -6,7 +6,7 @@
 @rem ##########################################################################
 
 @rem Set local scope for the variables with windows NT shell
-if "%OS%"=="Windows_NT" setlocal
+if "%OS%"=="Windows_NT" setlocal   
 
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.  
